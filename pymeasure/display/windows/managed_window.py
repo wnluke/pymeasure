3a77be472de81c3dd1644011c00e0a6f7a4fe0e5
Michele Sardo <michele.sardo@st.com>
2022-12-19 15:20:27 +0100

    Fixed merge error

--- pymeasure/display/windows/managed_window.py
+++ pymeasure/display/windows/managed_window.py
@@ -400,7 +400,7 @@ class ManagedWindowBase(QtWidgets.QMainWindow):
                 else:
                     results = Results.load(filename)
                     experiment = self.new_experiment(results)
-                    for curve in experiment.curve_list[0].values():
+                    for curve in experiment.curve_list:
                         if curve:
                             curve.update_data()
                     experiment.browser_item.progressbar.setValue(100)
