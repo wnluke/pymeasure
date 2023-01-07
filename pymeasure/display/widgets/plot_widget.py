e92d5cb5f5b463b74e9ebdcb06ad5bf014874876
Michele Sardo <msmttchr@gmail.com>
2023-01-07 17:51:21 +0100

    Fix lint errors

--- pymeasure/display/widgets/plot_widget.py
+++ pymeasure/display/widgets/plot_widget.py
@@ -34,10 +34,12 @@ from .plot_frame import PlotFrame
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
 
+
 class MultiResultsCurve(dict):
     def __getattr__(self, name):
         if name == "wdg":
             return list(self.values())[0].wdg
+
 
 class CheckableComboBox(QtWidgets.QComboBox):
     def __init__(self):
