6297a3d5e5be24cfe7ce89a48204f54c55319c5c
Brian DI GIORGIO <brian.digiorgio@st.com>
2023-07-17 11:43:10 +0200

    Third draft, not tested yet
    Signed-off-by: Brian DI GIORGIO <brian.digiorgio@st.com>

--- pymeasure/instruments/maurymw/xt981bl.py
+++ pymeasure/instruments/maurymw/xt981bl.py
@@ -23,7 +23,7 @@
 #
 
 from pymeasure.instruments import Instrument
-from pymeasure.instruments.validators import truncated_range, strict_discrete_set
+from pymeasure.instruments.validators import strict_discrete_set
 
 
 class XT981BL(Instrument):
@@ -107,28 +107,6 @@ class XT981BL(Instrument):
         """ Returns current fixture S-parameters """,
     )
 
-    ## To be changed with a method with parameters freqGHz_1 freqGHz_2 freqGHz_3
-    frequency = Instrument.control(
-        "FREQuency?;", "FREQuency %e;",
-        """ Displays currently loaded frequency and number of harmonics.
-            Sets tuner control frequency(s).
-            
-            FREQ <freqGHz> [nHarm] [nTune] Format for algorithm 2.
-            Where freqGHz, is the fundamental frequency, nHarm is the number of harmonics to observe, and nTune is number of harmonics to control.
-            Frequency must be a characterized frequency.
-            Example: FREQ 1.4 3 3
-            
-            FREQ <freqGHz_1> [freqGHz_2] [freqGHz_3] Format for algorithm 3.
-            Where freqGHz is a control frequency.
-            Example: FREQ 1.4 2.8 4.2
-            
-        This property can be set.
-        """,
-        validator=truncated_range,
-        values=(0.4, 8),
-        dynamic=True
-    )
-
     gamma = Instrument.control(
         "GAMMA?;", "GAMMA? %d;",
         """ Reads current Gamma and Loss(dB) of one or all control frequencies.
@@ -165,12 +143,6 @@ class XT981BL(Instrument):
             Returns: <Setup File> <Tuner File> <Fixture File> <Backport File> <Termination File> """,
     )
 
-    position = Instrument.measurement(
-        "POSition?;",
-        """ Query all files in current setup file.
-            Returns: <Setup File> <Tuner File> <Fixture File> <Backport File> <Termination File> """,
-    )
-
     spar = Instrument.measurement(
         "SPARameter?;",
         """ Reads current network S-Parameters and Loss(dB) of one or all control frequencies
@@ -184,8 +156,40 @@ class XT981BL(Instrument):
             Return value>0 ► Tuner is busy (bit0=carriage, bit 1=probe1, etc…) """,
     )
 
-    ## To be added: Setup store and setup recall and so on
+    init_status = Instrument.measurement(
+        "STATus:INIT?",
+        """ Indicates if one of all motors have been initialized. 1 indicates motor has been initialized """,
+    )
 
+    limit_status = Instrument.measurement(
+        "STATus:INIT?",
+        """ Indicates if one of all motors have been initialized. 1 indicates motor has been initialized """,
+    )
+
+    termination = Instrument.control(
+        "TERMination?;", "TERMination %s;",
+        """ Used to query current termination file.
+            Sets termination S-parameter block to a .s1p file or directly writes S-parameters of a selected control frequency.
+            If .s1p file is loaded freq must be called afterwards to set S-parameters.
+            If writing S-parameter data directly freq must be set first.
+            Example: term 7mm_term.s1p
+        This property can be set.
+        """,
+        dynamic=True
+    )
+
+    termination_spar = Instrument.measurement(
+        "TERMination:SPAR?;",
+        """ Returns the current termination S-parameters """,
+    )
+
+    vswr = Instrument.control(
+        "VSWR?;", "VSWR? %d;",
+        """ Reports VSWR and Loss.
+        Use IDX > 0 to report VSWR at specific control frequency. Blank IDX or IDX=0 reports VSWR for fundamental and all harmonics..
+        """,
+        dynamic=True
+    )
 
     def __init__(self, resourceName, description="Maury Microwave XT981BL", **kwargs):
         super().__init__(
@@ -204,14 +208,24 @@ class XT981BL(Instrument):
         self.write("INIT;")
         self.complete
 
-    def set_position(self, motor, position):
+    @property
+    def position(self):
+        """ Query all files in current setup file.
+            Returns: <Setup File> <Tuner File> <Fixture File> <Backport File> <Termination File> """
+        return self.ask("POSition?;")
+
+    @position.setter
+    def position(self, motor, position):
         """ Moves motors to target position
+
             Mot=1 ► Carriage
             Mot=2 ► Low frequency probe
             Mot=3 ► High frequency probe
+
             Examples:
                 POS 1 200 2 4500 3 2000
                 POS 2 3000
+
             param:
                 motor: integer that represents the motor
                 position: integer that represents the position
@@ -222,6 +236,70 @@ class XT981BL(Instrument):
         else:
             self.write(f"POSition {motor} {position};")
 
+    @property
+    def frequency(self):
+        """ Displays currently loaded frequency and number of harmonics. """
+        return self.ask("FREQuency?;")
+
+    @frequency.setter
+    def frequency(self, f1, f2="", f3=""):
+        """ Sets tuner control frequency(s).
+
+            FREQ <freqGHz> [nHarm] [nTune] Format for algorithm 2.
+            Where freqGHz, is the fundamental frequency, nHarm is the number of harmonics to observe, and nTune is number of harmonics to control.
+            Frequency must be a characterized frequency.
+            Example: FREQ 1.4 3 3
+
+            FREQ <freqGHz_1> [freqGHz_2] [freqGHz_3] Format for algorithm 3.
+            Where freqGHz is a control frequency.
+            Example: FREQ 1.4 2.8 4.2
+
+            param:
+                f1: floating that represents the control frequency 1 in GHz
+                f2: floating that represents the control frequency 2 in GHz
+                f3: floating that represents the control frequency 3 in GHz
+        """
+        self.write(f"FREQuency {f1} {f2} {f3};")
+
     def stop(self):
         """ Immediately stops all motor operations """
         self.write("STOP;")
+
+    def store_setup(self, filepath):
+        """ Save tuner setup file """
+        return self.write(f"SETUP:STORE {filepath}")
+
+    def recall_setup(self, filepath):
+        """ Load tuner setup file """
+        return self.write(f"SETUP:RECALL {filepath}")
+
+    def limit_status(self, carriage):
+        """ Reads status of limit switches for selected carriage. 1 indicates limit switch is currently triggered """
+        return self.ask(f"STATus:LIMIT? {carriage}")
+
+    def tune(self, mag, phase, mag2="", phase2="", mag3="", phase3=""):
+        """ Sets Gamma of FreqIdx (default 1) or of all control frequencies, and moves tuner """
+        self.write(f"TUNE {mag} {phase} {mag2} {phase2} {mag3} {phase3}")
+
+    def tune_vswr(self, vswr, phase, vswr2="", phase2="", vswr3="", phase3=""):
+        """ Sets VSWR of FreqIdx (default 1) or of all control frequencies, and moves tuner
+            Example: TUNE:VSWR 10 120 10 -120 10 0
+        """
+        self.write(f"TUNE:VSWR {vswr} {phase} {vswr2} {phase2} {vswr3} {phase3}")
+    
+    @property
+    def tune_weight(self):
+        """ Reports current tuning weight
+            Example: TUNE:WEIGHT 5 .003 1 """
+        return self.ask("TUNE:WEIGHT?")
+
+    @tune_weight.setter
+    def tune_weight(self, weight, radius, freq_idx):
+        """ Sets index for TUNE and TUNE:VSWR (default 1)
+            Example: TUNE:WEIGHT 5 .003 1 """
+        self.write(f"TUNE:WEIGHT {weight} {radius} {freq_idx}")
+
+    def reboot(self):
+        """Reboot Tuner"""
+        self.write("REBOOT;")
+
